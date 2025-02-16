773daf6403027013fa29a3fc3eda9e53d0aaec41
Amir011 <amircaballero2012@gmail.com>
2025-02-16 15:54:11 -0300

    Subiendo imagen

--- src/components/ItemsProductos.jsx
+++ src/components/ItemsProductos.jsx
@@ -1,17 +1,24 @@
-export function ItemsProductos({item}) {
+export function ItemsProductos({ item }) {
+  // Definir la imagen específica para los IDs 14 y 17
+  const nuevaImagen = item.id === 14 
+    ? "/assets/img/nueva-imagen14.png"
+    : item.id === 17 
+    ? "/assets/img/nueva-imagen17.png"
+    : item.img;
+
   return (
-    <article class="items__card">
+    <article className="items__card" data-sr-id={item.id}>
       <img
-        src={item.img}
+        src={nuevaImagen} // 🔹 Cambiamos la imagen solo en los IDs 14 y 17
         alt="items image"
-        class="items__img"
+        className="items__img"
       />
 
-      <h3 class="items__name">{item.title}</h3>
-      <span class="items__price">{item.precio}</span>
+      <h3 className="items__name">{item.title}</h3>
+      <span className="items__price">{item.precio}</span>
 
-      <button class="items__button">
-        <i class="ri-heart-3-line"></i>
+      <button className="items__button">
+        <i className="ri-heart-3-line"></i>
       </button>
     </article>
   );

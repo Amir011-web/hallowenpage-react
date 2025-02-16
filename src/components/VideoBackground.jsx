842ceaa90e3848ac1c935d2fc2712e77bda30752
Amir011 <amircaballero2012@gmail.com>
2025-02-16 02:23:00 -0300

    Subiendo imagen

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import videoSrc from "../assets/img/mi-video.mp4"; // Importa el video desde src
 import "../index.css"; // Asegúrate de que los estilos están importados
 
 const VideoBackground = () => {
   return (
     <div className="video-container">
       <video autoPlay loop muted playsInline>
-        <source src="/mi-video.mp4" type="video/mp4" />
+        <source src={videoSrc} type="video/mp4" />
         Tu navegador no soporta videos en HTML5.
       </video>
     </div>
